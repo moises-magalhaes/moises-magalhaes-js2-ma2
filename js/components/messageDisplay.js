ce9c1d0c485c722af937fe0aa75c9d04ba35edb9
moises-magalhaes <63241484+moises-magalhaes@users.noreply.github.com>
2021-02-20 13:02:44 +0100

    Update messageDisplay.js

--- js/components/messageDisplay.js
+++ js/components/messageDisplay.js
@@ -5,11 +5,3 @@ export function createMessage(
 	const html = `<div class="message ${type}">${message}</div>`;
 	return html;
 }
-
-/* const element = document.querySelector(".container");
-	const message = createMessage("empty", "Sorry! No products in the basket.");
-
-	console.log(element);
-
-	element.innerHTML = message;
-*/
