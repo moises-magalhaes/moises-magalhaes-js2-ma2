1d93939cf5cfbf464cd7314ad84a8be9b3f18332
moises-magalhaes <63241484+moises-magalhaes@users.noreply.github.com>
2021-03-26 11:36:59 +0100

    restarting

--- js/script.js
+++ js/script.js
@@ -1,24 +1,4 @@
 //import { books } from "./data/data.js";
-
-let books = [
-	{
-		isbn: "1600506460320",
-		title: "Great book",
-	},
-	{
-		isbn: "1600506460373",
-		title: "Ok book",
-	},
-	{
-		isbn: "1600506460521",
-		title: "Bad book",
-	},
-	{
-		isbn: "1600506460456",
-		title: "Terrible book",
-	},
-];
-
 console.log(books);
 
 function removeBook(removeIsbn) {
